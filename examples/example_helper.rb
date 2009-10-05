2dbbe4e46fa78caf016e7e56ec15321d2b1bb65a
Rob Sanheim <rsanheim@gmail.com>
2009-10-04 23:29:48 -0400

    chatterbox-email wired

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -1,17 +1,30 @@
-require 'rubygems'
+gem 'spicycode-micronaut'
 require 'micronaut'
+require 'mocha'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'chatterbox-email'
+require 'chatterbox'
+require 'chatterbox/email'
+
+ActionMailer::Base.delivery_method = :test
 
 def not_in_editor?
   !(ENV.has_key?('TM_MODE') || ENV.has_key?('EMACS') || ENV.has_key?('VIM'))
+end
+
+def valid_options(overrides = {})
+  options = { 
+    :message => { :summary => "here is a message" },
+    :config => { 
+      :to => "joe@example.com", 
+      :from => "someone@here.com" 
+    }
+  }.merge(overrides)
 end
 
 Micronaut.configure do |c|
   c.color_enabled = not_in_editor?
   c.filter_run :focused => true
 end
-
